f6db7ee5761a465b947791f7c288765464cce038
ateruel <ateruel@vivreweb.fr>
2016-06-13 15:14:29 +0200

    test

---
+++ js/config.js
@@ -0,0 +1,5 @@
+angular.module('beAppli.config', [])
+.constant('WORDPRESS_API_URL', 'http://www.beappli.fr/api/')
+.constant('WORDPRESS_PUSH_URL', 'http://www.beappli.fr/push/')
+.constant('GCM_SENDER_ID', '574597432927')
+;
---
+++ js/filters.js
@@ -0,0 +1,7 @@
+angular.module('beAppli.filters', [])
+
+.filter('rawHtml', ['$sce', function($sce){
+  return function(val) {
+    return $sce.trustAsHtml(val);
+  };
+}]);
